785f4a60728182e00b5acf8f02f014d0688070e3
Izael silva <izaell.oficial@gmail.com>
2024-06-12 21:32:20 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import { Form, FormDataProps } from "./components/Form"
+import { PrintPage } from "./components/Print"
+
+export function App() {
+  const [dadosFormulario, setDadosFormulario] = useState<FormDataProps | null>(null)
+
+  return (
+    <div className="max-w-2xl mx-auto pb-12 px-12">
+      {!dadosFormulario ? (
+        <Form onSubmit={(data) => setDadosFormulario(data)} />
+      ) : (
+        <PrintPage {...dadosFormulario} />
+      )}
+    </div>
+  )
+}
