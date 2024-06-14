b8d540b782a2ca1abcef9321cf2efe958ec36942
Izael silva <izaell.oficial@gmail.com>
2024-06-14 20:16:36 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { useState } from "react"
+
 import { Form, FormDataProps } from "./components/Form"
 import { PrintPage } from "./components/Print"
+import { Footer } from "./components/footer/Footer"
 
 export function App() {
   const [dadosFormulario, setDadosFormulario] = useState<FormDataProps | null>(null)
 
   return (
-    <div className="max-w-2xl mx-auto pb-12 px-12">
+    <div className="max-w-2xl mx-auto pb-12 px-12 ">
       {!dadosFormulario ? (
         <Form onSubmit={(data) => setDadosFormulario(data)} />
       ) : (
         <PrintPage {...dadosFormulario} />
       )}
+
+      <Footer />
     </div>
   )
 }
