45dc19d03c1e2dcfc15659b7acb24184c7fd5805
Ias4g <izaell.oficial@gmail.com>
2024-08-21 15:41:14 -0300

    Removing application version

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,18 +2,15 @@ export function Footer() {
   return (
     <footer className="flex items-center justify-center  mt-4 text-muted-foreground">
       <span className="flex-1 h-px bg-zinc-400" />
-      <div className="flex flex-col px-6 py-2 items-center justify-center border border-zinc-400 rounded-full">
-        <span>
-          Feito com 💕
-          <a
-            target="_blank"
-            className="ml-1 hover:underline"
-            href="https://github.com/ias4g"
-          >
-            Ias4g | Izael Silva
-          </a>
-        </span>
-        <span className="text-xs">v1.05</span>
+      <div className="flex flex-row px-6 py-2 items-center justify-center border border-zinc-400 rounded-full">
+        Feito com 💕
+        <a
+          target="_blank"
+          className="ml-1 hover:underline"
+          href="https://github.com/ias4g"
+        >
+          Ias4g | Izael Silva
+        </a>
       </div>
       <span className="flex-1 h-px bg-zinc-400" />
     </footer>
